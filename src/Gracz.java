943e66ba2f9759d031125b56cb4cc23656e04c53
jstefanowiczczop <jstefanowicz.czop@gmail.com>
2016-10-26 22:12:22 +0200

    Gracz

--- src/Gracz.java
+++ src/Gracz.java
@@ -1,5 +1,19 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 /**
  * Created by Jola on 2016-10-26.
  */
 public class Gracz {
+    int liczba = 0;
+    public void zaladuj(){
+        liczba = (int) (Match.random () *10);
+        System.out.println ("Typuje liczbę" + liczba);
+
+
+
+
+    }
+
+
+
 }
--- src/Zgadywanka.java
+++ src/Zgadywanka.java
@@ -29,6 +29,46 @@ public class Zgadywanka {
 
         while(true){
             System.out.println("Należy wytypować liczbę :" + liczbaodgadywana);
+
+            p1.zagaduj ();
+            p2.zagaduj ();
+            p3.zagaduj();
+
+            typp1=p1.liczba;
+            System.out.println ("Gracz pierwszy wytypował liczbę: +typ1");
+
+            typp2=p2.liczba;
+            System.out.println ("Gracz drugi wytypował liczbę: +typ2");
+
+            typp3=p3.liczba;
+            System.out.println ("Gracz trzeci wytypował liczbę: +typ13);
+
+                if (typp1 = LiczbaOdgadywana) {
+                poldgad1 = true;
+                (typp2 = LiczbaOdgadywana) {
+                    poldgad2 = true;
+
+                    (typp3 = LiczbaOdgadywana) {
+                        poldgad3 = true;
+                    }
+                    {if (p1odgad1 II p2odglad II p3odgadl){
+
+
+
+
+                    }
+                        System.out.println ("Mamy zwycięzcę");
+                        System.out,println("Czy gracz pierwszy wytypował poprawnie ? +p1odglad");
+                        System.out,println("Czy gracz drugo wytypował poprawnie ? +p2odglad");
+                        System.out,println("Czy gracz trzeci wytypował poprawnie ? + p3odglad");
+
+                        System.out.println("Koniec gry");
+
+
+            }
+else {
+                        System.out,println("Gracz musi spróbować jeszcze raz");
+                    }
         }
 
     }
