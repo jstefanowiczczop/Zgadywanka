39e36ef1a7f8f631142ab07adc5df82b7e872c7c
jstefanowiczczop <jstefanowicz.czop@gmail.com>
2016-10-26 21:35:59 +0200

    Zgadywanka

--- src/Zgadywanka.java
+++ src/Zgadywanka.java
@@ -2,4 +2,21 @@
  * Created by Jola on 2016-10-26.
  */
 public class Zgadywanka {
+
+        Gracz p1;
+        Gracz p2;
+    Gracz p3;
+
+    public void rozpocznijGre(){
+
+        p1 = new Gracz ();
+        p2 = new Gracz ();
+        p3 = new Gracz ();
+    }
+
+
+
 }
+
+
+
