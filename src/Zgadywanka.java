440325741d744085fc9fbf665dbaae87d1ee39f0
jstefanowiczczop <jstefanowicz.czop@gmail.com>
2016-10-26 21:47:16 +0200

    Zgadywanka

--- src/Zgadywanka.java
+++ src/Zgadywanka.java
@@ -12,6 +12,25 @@ public class Zgadywanka {
         p1 = new Gracz ();
         p2 = new Gracz ();
         p3 = new Gracz ();
+
+
+
+        int typp1 = 0;
+        int typp2 = 0;
+        int typp3 = 0;
+
+
+        boolean p1odgad1 = false;
+        boolean p1odgad2 = false;
+        boolean p1odgad3 = false;
+
+      int.liczbaOdgadywana = (int)(Math.random() *10);
+        System.out.println("Myślę o liczbie z zakresu od 0 do 9....");
+
+        while(true){
+            System.out.println("Należy wytypować liczbę :" + liczbaodgadywana);
+        }
+
     }
 
 
