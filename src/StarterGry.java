ff1ba213cc2295e8017660ce797f72fbbf584269
jstefanowiczczop <jstefanowicz.czop@gmail.com>
2016-10-26 22:14:47 +0200

    Gracz

--- src/StarterGry.java
+++ src/StarterGry.java
@@ -2,4 +2,13 @@
  * Created by Jola on 2016-10-26.
  */
 public class StarterGry {
+
+
+    public static void main (String [] args){
+        Zgadywanka gra= new Zgadywanka();
+        gra.rozpocznijGre();
+
+
+
+    }
 }
